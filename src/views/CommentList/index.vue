c991a8035190c5536c05332feab2606aea8ad04d
Y.Wang <shengguanglong01@sina.com>
2019-04-28 19:33:57 +0800

    comment function

--- src/views/CommentList/index.vue
+++ src/views/CommentList/index.vue
@@ -84,11 +84,12 @@
             <el-button type="primary" size="mini" icon="el-icon-edit" @click="edit(scope.row)"/>
             <el-dialog :visible.sync="dialogFormVisible" title="编辑评论">
               <el-form label-width="120px" style="margin: 8px">
-                <el-form-item label="昵称" style="text-align: left">{{ scope.row.name }}</el-form-item>
+                <el-form-item label="头像" style="text-align: left"><el-input v-model="logo" placeholder="请输入头像"/></el-form-item>
+                <el-form-item label="昵称" style="text-align: left"><el-input v-model="name" placeholder="请输入昵称"/></el-form-item>
                 <el-form-item label="楼层" style="text-align: left">{{ scope.row.level }}</el-form-item>
                 <el-form-item label="评论时间" style="text-align: left">{{ scope.row.date }}</el-form-item>
-                <el-form-item label="所在地" style="text-align: left">{{ scope.row.location }}</el-form-item>
-                <el-form-item label="机型" style="text-align: left">{{ scope.row.model }}</el-form-item>
+                <el-form-item label="所在地" style="text-align: left"><el-input v-model="location" placeholder="请输入所在地"/></el-form-item>
+                <el-form-item label="机型" style="text-align: left"><el-input v-model="model" placeholder="请输入机型"/></el-form-item>
                 <el-form-item label="评论内容"><el-input v-model="commenttext" :autosize="{ minRows: 5, maxRows: 10}" type="textarea" placeholder="请输入评论"/></el-form-item>
                 <el-form-item label="点赞数"><el-input v-model="likenum" placeholder="请输入点赞数"/></el-form-item>
                 <el-form-item>
@@ -108,12 +109,20 @@
 
 <script>
 import axios from 'axios'
+import qs from 'qs'
 export default {
   data() {
     return {
       list: [],
       listLoading: true,
       dialogFormVisible: false,
+      id: '',
+      type: '',
+      level: 0,
+      name: '',
+      logo: '',
+      location: '',
+      model: '',
       commenttext: '',
       likenum: 0
     }
@@ -136,15 +145,36 @@ export default {
     },
     edit(res) {
       this.dialogFormVisible = true
-      this.likenum = res.like
+      this.id = res._id
+      this.type = res.type
+      this.level = res.level
+      this.name = res.name
+      this.logo = res.head
+      this.location = res.location
+      this.model = res.model
       this.commenttext = res.text
+      this.likenum = res.like
     },
     confirmEdit() {
-      this.$notify({
-        type: 'success',
-        message: '编辑成功!'
+      var that = this
+      axios.post('/server/updateComment/', qs.stringify({
+        id: that.id,
+        type: that.type,
+        level: that.level,
+        name: that.name,
+        logo: that.logo,
+        location: that.location,
+        model: that.model,
+        text: that.commenttext,
+        like: that.likenum
+      })).then(function(res) {
+        that.$notify({
+          type: 'success',
+          message: '编辑成功!'
+        })
+        that.dialogFormVisible = false
+        that.fetchData()
       })
-      this.dialogFormVisible = false
     },
     del(res) {
       this.$confirm('此操作将永久删除, 是否继续?', '提示', {
